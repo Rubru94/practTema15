5ac34c89fd2081ed4d929bd6444f9744b657851f
Rubru94 <rubenru94@hotmail.com>
2020-06-06 14:21:15 +0200

    Delete obtainVersionPom.sh

--- obtainVersionPom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-$1 = $(mvn -q \
-        -Dexec.executable="echo" \
-        -Dexec.args='${project.version}' \
-        --non-recursive \
-        org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
